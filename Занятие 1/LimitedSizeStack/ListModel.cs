ecc50b0f1125ee7fa820f44842a9848bc88d243a
rallent-q <maksfesukm0@mail.ru>
2025-02-27 00:01:43 +0300

    Update ListModel.cs

--- Занятие 1/LimitedSizeStack/ListModel.cs
+++ Занятие 1/LimitedSizeStack/ListModel.cs
@@ -1,40 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace LimitedSizeStack;
-
-public class ListModel<TItem>
+namespace LimitedSizeStack
 {
-	public List<TItem> Items { get; }
-	public int UndoLimit;
-        
-	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
-	{
-	}
+    public class ListModel<TItem>
+    {
+        private readonly Stack<TItem> _history; // Stack to keep track of added items
+        public List<TItem> Items { get; }
+        public int UndoLimit { get; }
 
-	public ListModel(List<TItem> items, int undoLimit)
-	{
-		Items = items;
-		UndoLimit = undoLimit;
-	}
+        public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+        {
+            _history = new Stack<TItem>();
+        }
 
-	public void AddItem(TItem item)
-	{
-		Items.Add(item);
-	}
+        public ListModel(List<TItem> items, int undoLimit)
+        {
+            Items = items ?? throw new ArgumentNullException(nameof(items));
+            UndoLimit = undoLimit > 0 ? undoLimit : throw new ArgumentOutOfRangeException(nameof(undoLimit), "Undo limit must be greater than zero.");
+            _history = new Stack<TItem>();
+        }
 
-	public void RemoveItem(int index)
-	{
-		Items.RemoveAt(index);
-	}
+        public void AddItem(TItem item)
+        {
+            if (Items.Count >= UndoLimit)
+            {
+                // Remove the oldest item (first in the list)
+                Items.RemoveAt(0);
+            }
 
-	public bool CanUndo()
-	{
-		return false;
-	}
+            Items.Add(item);
+            _history.Push(item); // Track the added item for undo
+        }
 
-	public void Undo()
-	{
-		throw new NotImplementedException();
-	}
+        public void RemoveItem(int index)
+        {
+            if (index < 0 || index >= Items.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+            }
+
+            // If we are removing an item, we should also remove it from history if it was the last added item
+            var removedItem = Items[index];
+            Items.RemoveAt(index);
+
+            // Only remove from history if we are removing the most recent item
+            if (_history.Count > 0 && EqualityComparer<TItem>.Default.Equals(_history.Peek(), removedItem))
+            {
+                _history.Pop();
+            }
+        }
+
+        public bool CanUndo()
+        {
+            return _history.Count > 0;
+        }
+
+        public void Undo()
+        {
+            if (!CanUndo())
+            {
+                throw new InvalidOperationException("No actions to undo.");
+            }
+
+            TItem lastAdded = _history.Pop();
+            Items.Remove(lastAdded); // Remove the last added item from the list
+        }
+    }
 }
