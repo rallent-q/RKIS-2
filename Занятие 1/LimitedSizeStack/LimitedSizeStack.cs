6e4210ab3e95913ed322375ce5589f93b0904bf6
rallent-q <maksfesukm0@mail.ru>
2025-02-26 23:48:07 +0300

    Update LimitedSizeStack.cs

--- Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
+++ Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
@@ -1,22 +1,46 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-namespace LimitedSizeStack;
-
-public class LimitedSizeStack<T>
+namespace LimitedSizeStack
 {
-	public LimitedSizeStack(int undoLimit)
-	{
-	}
+    public class LimitedSizeStack<T>
+    {
+        private readonly int _undoLimit;
+        private readonly LinkedList<T> _stack;
 
-	public void Push(T item)
-	{
-		throw new NotImplementedException();
-	}
+        public LimitedSizeStack(int undoLimit)
+        {
+            if (undoLimit <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(undoLimit), "Undo limit must be greater than zero.");
+            }
 
-	public T Pop()
-	{
-		throw new NotImplementedException();
-	}
+            _undoLimit = undoLimit;
+            _stack = new LinkedList<T>();
+        }
 
-	public int Count => throw new NotImplementedException();
+        public void Push(T item)
+        {
+            if (_stack.Count == _undoLimit)
+            {
+                // Remove the oldest item (the last one in the LinkedList)
+                _stack.RemoveLast();
+            }
+            _stack.AddFirst(item); // Add the new item to the top of the stack
+        }
+
+        public T Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            T value = _stack.First.Value; // Get the top item
+            _stack.RemoveFirst(); // Remove the top item
+            return value; // Return the popped item
+        }
+
+        public int Count => _stack.Count; // Return the current count of items in the stack
+    }
 }
