de9f399fe6e35aae068e16a64c59ae52f9f7f960
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-03-06 19:22:50 +0300

    Lesson 2: MovingMaxTask.cs

--- Занятие 2/Smooth/MovingMaxTask.cs
+++ Занятие 2/Smooth/MovingMaxTask.cs
@@ -8,7 +8,24 @@ public static class MovingMaxTask
 {
 	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		//Fix me!
-		return data;
+		var window = new Queue<double>();
+        	var list = new LinkedList<double>();
+           	foreach (var point in data)
+        	{
+                	window.Enqueue(point.OriginalY);
+                	if (window.Count > windowWidth)
+                	{
+                    		if (window.Dequeue() >= list.First.Value)
+                    		{
+                        		list.RemoveFirst();
+                    		}
+                	}
+                	while (list.Count != 0 && point.OriginalY > list.Last.Value)
+                	{
+                    		list.RemoveLast();
+                	}
+                	list.AddLast(point.OriginalY);
+                	yield return point.WithMaxY(list.First.Value);
+		}
 	}
 }
