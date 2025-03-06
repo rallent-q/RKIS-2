5d80f15b429f181803ab18044bf8243993e81cc5
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-03-06 19:15:09 +0300

    Lesson 2: ExpSmoothingTask.cs

--- Занятие 2/Smooth/ExpSmoothingTask.cs
+++ Занятие 2/Smooth/ExpSmoothingTask.cs
@@ -5,8 +5,23 @@ namespace yield;
 public static class ExpSmoothingTask
 {
 	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
-	{
-		//Fix me!
-		return data;
+        {
+		var value = 0.0;
+		var isCheked = false;
+		foreach (var point in data)
+		{
+			var item = new DataPoint(point);
+                	if (isCheked)
+                	{
+                    		item = point.WithExpSmoothedY(value + alpha * (point.OriginalY - value));
+                	}
+                	else
+                	{
+                    		item = point.WithExpSmoothedY(point.OriginalY);
+                    		isCheked = true;
+                	}
+                	yield return item;
+                	value = item.ExpSmoothedY;
+            	}
 	}
 }
