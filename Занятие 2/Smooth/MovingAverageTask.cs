dc4c864f95cc099773a719747284993cb2837ebf
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-03-06 19:19:53 +0300

    Lesson 2: MovingAverageTask.cs

--- Занятие 2/Smooth/MovingAverageTask.cs
+++ Занятие 2/Smooth/MovingAverageTask.cs
@@ -6,7 +6,18 @@ public static class MovingAverageTask
 {
 	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		//Fix me!
-		return data;
+		var value = 0.0d;
+        	var window = new Queue<double>();
+            	foreach (var point in data)
+           	{
+                	if (window.Count >= windowWidth)
+                	{
+                    		value -= window.Dequeue();
+                	}
+			window.Enqueue(point.OriginalY);
+                	value += point.OriginalY;
+                	var item = point.WithAvgSmoothedY(value / window.Count);
+			yield return item;
+		}
 	}
 }
