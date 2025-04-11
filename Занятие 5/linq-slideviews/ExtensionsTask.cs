31a4d8e5c88e39b9e15a41782c632b67d7efb4b4
rallent-q <maksfesukm0@mail.ru>
2025-04-11 17:14:21 +0300

    Lesson 5: ExtensionsTask

--- Занятие 5/linq-slideviews/ExtensionsTask.cs
+++ Занятие 5/linq-slideviews/ExtensionsTask.cs
@@ -1,28 +1,38 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace linq_slideviews;
-
-public static class ExtensionsTask
+namespace linq_slideviews
 {
-	/// <summary>
-	/// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
-	/// Медиана списка из четного количества элементов — это среднее арифметическое 
-    /// двух серединных элементов списка после сортировки.
-	/// </summary>
-	/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
-	public static double GetMedian(this IEnumerable<double> items)
-	{
-		throw new NotImplementedException();
-	}
-
-	/// <returns>
-	/// Возвращает последовательность, состоящую из пар соседних элементов.
-	/// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
-	/// </returns>
-	public static IEnumerable<(T First, T Second)> GetBigrams<T>(this IEnumerable<T> items)
-	{
-		throw new NotImplementedException();
-	}
+    public static class ExtensionsTask
+    {
+        public static double GetMedian(this IEnumerable<double> items)
+        {
+            var listOfItems = items.ToList();
+            listOfItems.Sort();
+            if (listOfItems.Count == 0)
+                throw new InvalidOperationException();
+            else if (listOfItems.Count % 2 == 1)
+                return listOfItems[listOfItems.Count / 2];
+            else
+                return (listOfItems[listOfItems.Count / 2 - 1] + listOfItems[listOfItems.Count / 2]) / 2;
+        }
+        public static IEnumerable<Tuple<T, T>> GetBigrams<T>(this IEnumerable<T> items)
+        {
+            T tempFirst = default(T);
+            bool isTempTupleEmpty = true;
+            foreach (var item in items)
+            {
+                if (isTempTupleEmpty)
+                {
+                    tempFirst = item;
+                    isTempTupleEmpty = false;
+                }
+                else
+                {
+                    yield return new Tuple<T, T>(tempFirst, item);
+                    tempFirst = item;
+                }
+            }
+        }
+    }
 }
