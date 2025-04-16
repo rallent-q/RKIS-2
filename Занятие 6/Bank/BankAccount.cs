e84b626ac436cb61e9206dd9257c018828001ca0
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-04-16 21:08:59 +0300

    Lesson 6: BankAccount.cs

--- Занятие 6/Bank/BankAccount.cs
+++ Занятие 6/Bank/BankAccount.cs
@@ -1,25 +1,57 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bank
 {
 	public class BankAccount
 	{
+		private decimal _balance
+		private readonly object _lock = new object();
+		
 		public Guid Id { get; } = Guid.NewGuid();
 
 		public decimal GetBalance()
 		{
-			throw new NotImplementedException();
+			lock (_lock)
+            		{
+                		return _balance;
+            		}
 		}
 
 		public async Task DepositAsync(decimal amount)
 		{
-			throw new NotImplementedException();
+			if (amount <= 0)
+            		{
+                		throw new ArgumentException("Amount must be positive", nameof(amount));
+            		}
+
+	            	await Task.Delay(100);
+
+            		lock (_lock)
+            		{
+                		_balance += amount;
+            		}
 		}
 
 		public async Task WithdrawAsync(decimal amount)
 		{
-			throw new NotImplementedException();
+			if (amount <= 0)
+            		{
+                		throw new ArgumentException("Amount must be positive", nameof(amount));
+            		}
+
+            		await Task.Delay(100); // Имитация долгих вычислений
+
+            		lock (_lock)
+            		{
+                		if (_balance < amount)
+                		{
+                    			throw new InvalidOperationException("Insufficient funds");
+                		}
+
+                		_balance -= amount;
+            		}
 		}
 	}
 }
