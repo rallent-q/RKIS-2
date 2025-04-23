5a6586fd8f525d4fb09ae5bc3849de0ebfa9bc1b
rallent-q <maksfesukm0@mail.ru>
2025-04-23 21:49:57 +0300

    lesson 7: AesCrypt

--- Занятие 7/cripto-reader/AesCrypt.cs
+++ Занятие 7/cripto-reader/AesCrypt.cs
@@ -1,15 +1,67 @@
-﻿namespace cripto_reader
-{
-	static class AesCrypt
-	{
-		public static byte[] Encrypt(string plainText, string password)
-		{
-			throw new NotImplementedException();
-		}
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
-		public static string Decrypt(byte[] cipherText, string password)
-		{
-			throw new NotImplementedException();
-		}
-	}
+namespace cripto_reader
+{
+    static class AesCrypt
+    {
+        public static byte[] Encrypt(string plainText, string password)
+        {
+            // Создаем ключ и IV на основе пароля
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                byte[] iv = new byte[16]; // IV должен быть 16 байт для AES
+                Buffer.BlockCopy(key, 0, iv, 0, iv.Length);
+
+                using (var aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.IV = iv;
+
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            using (var writer = new StreamWriter(cryptoStream))
+                            {
+                                writer.Write(plainText);
+                            }
+                            return memoryStream.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(byte[] cipherText, string password)
+        {
+            // Создаем ключ и IV на основе пароля
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                byte[] iv = new byte[16]; // IV должен быть 16 байт для AES
+                Buffer.BlockCopy(key, 0, iv, 0, iv.Length);
+
+                using (var aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.IV = iv;
+
+                    using (var memoryStream = new MemoryStream(cipherText))
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                        {
+                            using (var reader = new StreamReader(cryptoStream))
+                            {
+                                return reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
