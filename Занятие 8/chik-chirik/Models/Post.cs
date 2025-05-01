f9e39fdb20665272b22530c9d54e5caf7c40072c
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-05-01 18:37:24 +0300

    Lesson 8: Post.cs

--- Занятие 8/chik-chirik/Models/Post.cs
+++ Занятие 8/chik-chirik/Models/Post.cs
@@ -2,5 +2,10 @@
 {
 	public class Post
 	{
+		public int Id { get; set; }
+        	public int UserId { get; set; }
+        	public string Title { get; set; }
+       		public string Body { get; set; }
+        	public User User { get; set; }
 	}
 }
