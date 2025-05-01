9a36a2336e022b5cf295a706baf484966281f09c
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-05-01 18:36:32 +0300

    Lesson 8: Comment.cs

--- Занятие 8/chik-chirik/Models/Comment.cs
+++ Занятие 8/chik-chirik/Models/Comment.cs
@@ -2,5 +2,10 @@
 {
 	public class Comment
 	{
+		public int Id { get; set; }
+        	public int PostId { get; set; }
+        	public string Name { get; set; }
+        	public string Email { get; set; }
+        	public string Body { get; set; }
 	}
 }
