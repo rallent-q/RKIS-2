e24a9e43c513c49685de17cf18a3ed698c7c0f50
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2025-05-01 18:33:17 +0300

    Lesson 8: ApiService.cs

--- Занятие 8/chik-chirik/ApiService.cs
+++ Занятие 8/chik-chirik/ApiService.cs
@@ -1,19 +1,48 @@
-﻿using System.Net.Http.Json;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 using chik_chirik.Models;
 
 namespace chik_chirik
 {
-	public class ApiService
-	{
-		public async Task<List<Post>> GetPostsAsync(string searchTerm = null)
-		{
-			throw new NotImplementedException();
-		}
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
 
-		// Получить комментарии к посту  
-		public async Task<List<Comment>> GetCommentsAsync(int postId)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            if (_httpClient.BaseAddress == null)
+                _httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+        }
+
+        public async Task<List<Post>> GetPostsAsync(string searchTerm = null)
+        {
+            var posts = await _httpClient.GetFromJsonAsync<List<Post>>("posts");
+            var users = await _httpClient.GetFromJsonAsync<List<User>>("users");
+
+            foreach (var post in posts)
+            {
+                post.User = users.FirstOrDefault(u => u.Id == post.UserId);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                posts = posts
+                    .Where(p => p.User != null &&
+                                p.User.Username.Equals(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+
+            return posts;
+        }
+
+        public async Task<List<Comment>> GetCommentsAsync(int postId)
+        {
+            return await _httpClient.GetFromJsonAsync<List<Comment>>($"posts/{postId}/comments");
+        }
+    }
 }
