64e96760556905b10fa6aa2b8a61e244f8ca297d
rallent-q <maksfesukm0@mail.ru>
2025-05-07 22:13:31 +0300

    lesson 9: make TodoList

--- Занятие 9/TodoList/TodoItem.cs
+++ Занятие 9/TodoList/TodoItem.cs
@@ -1,11 +1,11 @@
-﻿namespace TodoList
+namespace TodoList
 {
-	public class TodoItem
-	{
-		public Guid Id { get; } = Guid.NewGuid();
-		public string Text { get; set; } = "";
-		public DateTime StartTime { get; } = DateTime.Now;
-		public DateTime? EndTime { get; set; 
-		public bool IsCompleted { get; set; }
-	}
+    public class TodoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Text { get; set; } = "";
+        public DateTime StartTime { get; set; } = DateTime.Now;
+        public DateTime? EndTime { get; set; }
+        public bool IsCompleted { get; set; }
+    }
 }
