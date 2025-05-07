47a7528142fa7b28a4c9efe191d810dd29802293
rallent-q <maksfesukm0@mail.ru>
2025-05-07 22:11:25 +0300

    lesson 9: make TodoList

--- Занятие 9/TodoList/AppDbContext.cs
+++ Занятие 9/TodoList/AppDbContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoList
 {
-	public class AppDbContext : DbContext
-	{
-	}
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=todolist.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(t => t.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
 }
