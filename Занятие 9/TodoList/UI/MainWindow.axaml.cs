807b5520dfdf7b862f5757bf8eab0171fec910b8
rallent-q <maksfesukm0@mail.ru>
2025-05-07 22:09:42 +0300

    lesson 9: make TodoList

--- Занятие 9/TodoList/UI/MainWindow.axaml.cs
+++ Занятие 9/TodoList/UI/MainWindow.axaml.cs
@@ -1,4 +1,4 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Collections.ObjectModel;
 
@@ -55,7 +55,7 @@ namespace TodoList.UI
 			if (sender is Button btn && btn.DataContext is TodoItemWithEditing item)
 			{
 				if (item.IsEditing)
-					await _todoService.UpdateTodo(item.Todo);
+					await _todoService.UpdateTodo(item.Todo.Id, item.Todo.Text);
 
 				item.IsEditing = !item.IsEditing;
 				RefreshItem(item);
@@ -68,7 +68,7 @@ namespace TodoList.UI
 			{
 				item.Todo.IsCompleted = checkBox.IsChecked ?? false;
 				item.Todo.EndTime = item.Todo.IsCompleted ? DateTime.Now : null;
-				await _todoService.UpdateTodo(item.Todo);
+				await _todoService.UpdateTodo(item.Todo.Id, item.Todo.Text);
 				RefreshItem(item);
 			}
 		}
@@ -80,4 +80,4 @@ namespace TodoList.UI
 			_todos[index] = item;
 		}
 	}
-}
+}	
